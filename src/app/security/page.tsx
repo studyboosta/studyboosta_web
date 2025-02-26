841900ed3cf87fd41dba44815495e19ff2b74032
igbafe peter <igbafes8@gmail.com>
2025-02-26 08:44:16 +0100

    feat: security and marketplace

---
+++ src/app/security/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Security = () => {
+  return (
+    <div className='h-screen items-center flex justify-center'>
+      <p className='text-[#050C9C] sm:text-5xl text-2xl font-bold'>Coming Soon</p>
+    </div>
+  )
+}
+
+export default Security
