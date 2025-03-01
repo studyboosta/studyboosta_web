57198a0bf8cf79b277bb73c3401a075e5160bbba
Shallyeneje <Shalomenje2@gmail.com>
2025-03-01 01:52:19 +0100

    Fix unescaped entities in JSX

--- src/app/marketplace/page.tsx
+++ src/app/marketplace/page.tsx
@@ -11,15 +11,16 @@ const ComingSoon = () => {
 
       {/* Subtitle */}
       <p className="text-gray-600 text-lg sm:text-xl mt-4 max-w-2xl">
-        We're crafting something amazing in a very short while, we are bringing together expert video lessons, high-income digital skills,
-        mentorship, scholarships, and internship updates – all in one place.
+        We&apos;re crafting something amazing in a very short while, we are
+        bringing together expert video lessons, high-income digital skills,
+        mentorship, scholarships, and internship updates all in one place.
       </p>
 
       {/* Button */}
       <a href="https://chat.whatsapp.com/I2FEWTyTANI5F3sVWRQJD8">
-      <button className="mt-6 flex items-center gap-2 bg-[#050C9C] text-white px-6 py-3 rounded-lg text-lg font-bold hover:bg-blue-800 transition">
-        <FaBolt /> Stay Notified
-      </button>
+        <button className="mt-6 flex items-center gap-2 bg-[#050C9C] text-white px-6 py-3 rounded-lg text-lg font-bold hover:bg-blue-800 transition">
+          <FaBolt /> Stay Notified
+        </button>
       </a>
 
       {/* Footer */}
